5a382b4d4db23be11d6f875f2e635abd3ade6d6a
leobar37 <usatloqueando@gmail.com>
2020-04-16 08:09:38 -0500

    segundo commit

--- src/app/pages/page1/page1.component.ts
+++ src/app/pages/page1/page1.component.ts
@@ -1,56 +1,105 @@
-import { Component, OnInit } from '@angular/core';
-import { cargarEstilos, cargarScripts } from 'src/scripts/dependencias';
-import jump from 'jump.js';
-import { activar_modal, desactivar_modal } from '../../../scripts/alternos';
+import { Component, OnInit } from "@angular/core";
+import { cargarEstilos, cargarScripts } from "src/scripts/dependencias";
+import jump from "jump.js";
+import { activar_modal, desactivar_modal, animateCSS } from '../../../scripts/alternos';
+
 declare function stickyStack();
-declare var stackedCards:any;
-declare function iconate(valor1 :any , valor2:any):any;
+declare var stackedCards: any;
+declare function iconate(valor1: any, valor2: any): any;
 @Component({
-  selector: 'app-page1',
-  templateUrl: './page1.component.html',
-  styleUrls: ['./page1.component.scss']
+  selector: "app-page1",
+  templateUrl: "./page1.component.html",
+  styleUrls: ["./page1.component.scss"],
 })
 export class Page1Component implements OnInit {
-  public loading:boolean =true;
-  private scripts:string[] = [
+  public loading: boolean = true;
+  private scripts: string[] = [
     "assets/libs/slider/stackedCards.min.js",
     "assets/libs/sticky/stycky.js",
-   "assets/libs/iconate/iconate.js"
-  ]
-  private estilos :string[] = [
+    "assets/libs/iconate/iconate.js",
+  ];
+  private estilos: string[] = [
     "assets/libs/slider/stackedCards.css",
     "assets/pages/pageone.css",
     "assets/libs/iconate/iconate.min.css",
-  ]
-  constructor() {
-  }
+  ];
+  constructor() {}
   async ngOnInit() {
-  activar_modal();
-  await cargarEstilos(this.estilos ,'page1');
-  await cargarScripts(this.scripts, 'page1');
-  var stackedCard = new stackedCards({
-    selector: ".stacked-cards",
-    layout: "slide",
-    transformOrigin: "bottom",
-  });
-    stackedCard.init(); 
-  let elemento:any=  $('.main-content-wrapper');
-  elemento.stickyStack();
-  desactivar_modal();
-  this.loading = false;
-   //next pagem 
-    let  $nextPage =  $('.nextaPage');  
-     $nextPage.on('click' , ()=>{
-       //manejar el scrooll
-       window.scroll({
-        top: 1000,
-        behavior: 'smooth'
-       })
-    }) 
-    $(window).on('scroll', function() {
-    console.log('scroll');
-    
-    });
-
+    activar_modal();
+    await cargarEstilos(this.estilos, "page1");
+    await cargarScripts(this.scripts, "page1");
+    this.animacionesScroll();
+    desactivar_modal();
+    this.loading = false;
   }
+  animacionesScroll() {
+    
+    //animaciones
+    //menu animacion
+    animateCSS('.content' ,'bounceInRight')
+    animateCSS('.menu' ,'bounceInLeft', null ,()=>{
+      // animateCSS('.menu' ,'bounceInDown');
+      // animateCSS('.cont_texto' ,'bounceInDown');
+    });
+    //desactivar el scroll
+    //efcto de tarjetas
+    var stackedCard = new stackedCards({
+      selector: ".stacked-cards",
+      layout: "slide",
+      transformOrigin: "bottom",
+    });
+    stackedCard.init();
+    let elemento: any = $(".main-content-wrapper");
+    elemento.stickyStack();
+    //fin
+    //pagina siguiente
+    let $nextPage = $(".nextaPage");
+    let secciones = $('.main-content-wrapper section'); 
+    let cont =  0;
+    $nextPage.on("click", () => {
+      //manejar el scrooll
+       cont+=1000; 
+       let $next = $('.nextaPage i');
+        if(cont >= ((secciones.length * 1000) - 2000)){
+           $next.removeClass('fa-arrow-right');
+           $next.addClass('fa-angle-double-up');
+          }
+          if(cont >= ((secciones.length * 1000) -1000)){
+            cont =0;   
+            $next.removeClass('fa-angle-double-up');
+             setTimeout(()=>{
+              $next.addClass('fa-arrow-right');
+             } , 1000)
+         }
+      let cantScroll = $(window).scrollTop();
+       let cantSumar = cont- cantScroll;
+       window.scroll({
+        top: cantScroll+ cantSumar,
+        behavior: "smooth",
+      });
+    });
+    //fin
+    //scrooll
+    const scrollMove = (element :HTMLElement , referencia:HTMLElement ) =>{
+      const refElment =  $(referencia);
+      element.addEventListener('click' , ()=>{
+        window.scroll({ top:  (refElment.offset().top + refElment.height() + 100), behavior :'smooth'})
+      })
+    }   
+    let itemsMenu =  $('.menu li');
+   
+  for (let i = 1; i < itemsMenu.length ; i++) {
+     scrollMove(itemsMenu[i] ,  secciones[i]);
+  }
+  //pasar pagina   boton
+  let $btnSiguiente = $('.nextaPage');
+ $(window).scroll(  ()=>{
+      let posBarra = $(window).scrollTop();
+       $btnSiguiente.css( {
+          'transform': 'translate(0,' + posBarra * 1+'px)'
+       })
+ }) 
+}
+ 
+
 }
--- src/scripts/alternos.ts
+++ src/scripts/alternos.ts
@@ -1,18 +1,43 @@
 export const activar_modal = ()=>{
     let load = $('#load');
-    // load.removeClass('esconderload');
      if(!load.hasClass('load')){
          load.addClass('load')
      };
-    
     }
     
 export const desactivar_modal = ()=>{
         let load = $('#load');
         load.removeClass('load');
-        // load.fadeOut('slow');
         let body =  document.body;
         body.style.overflowY = 'visible';
         load.addClass('esconderload');
-          
+}
+
+export function animateCSS(element:string, animationName:string, elhtml?:HTMLElement, callback?:Function ) {
+  let node;
+    if(elhtml){
+      node =  elhtml;   
+    }else{
+     node  = document.querySelector(element);
+    }
+    node.classList.add('animated', animationName)
+    function handleAnimationEnd() {
+        node.classList.remove('animated', animationName)
+        node.removeEventListener('animationend', handleAnimationEnd)
+        if (typeof callback === 'function') callback()
+    }
+    node.addEventListener('animationend', handleAnimationEnd)
+}
+export function getViewPortHeight() {
+	var viewportheight;
+	if (typeof window.innerWidth != 'undefined') {
+	    viewportheight = window.innerHeight;
+		// IE6 in standards compliant mode (i.e. with a valid doctype as the first line in the document)
+	} else if (typeof document.documentElement != 'undefined'   && typeof document.documentElement.clientWidth != 'undefined' && document.documentElement.clientWidth != 0) {
+       viewportheight = document.documentElement.clientHeight;
+	} else {
+		viewportheight = document.getElementsByTagName('body')[0].clientHeight;
+	}
+	return viewportheight;
+		
 }
